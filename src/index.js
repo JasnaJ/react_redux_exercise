38c141cc99406b76ba41296ea697804a0d581b90
JasnaJ <jovanic_jasna@yahoo.com>
2022-03-09 12:23:19 +0100

    store is working

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
 
+const store = configureStore();
+//ako zelimo da postavimo neko inicijalno stanje, tipa punimo store iz nekog internog dok., to radimo ovde
+//u reduceru se postavlja inicijalno stanje na prazan niz, a ovako ce se taj naiy odmah popuniti podacima
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById("app")
 );
